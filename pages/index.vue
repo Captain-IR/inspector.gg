cbbf0dcf7f2b36fabc698ca7b3842c5c9697753a
Captain IR <52345388+Captain-IR@users.noreply.github.com>
2020-09-29 20:46:39 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <h1>Home Page</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.container {
+  @apply min-h-screen p-6 mx-auto;
+}
+</style>
